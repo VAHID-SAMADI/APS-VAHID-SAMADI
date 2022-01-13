1479a6e0b4f27060d61d113c2f1c7a767f8f33b6
Mr. VaHiD Samadi <93014357+VAHID-SAMADI@users.noreply.github.com>
2022-01-14 03:57:11 +0800

    day 2 first commit

---
+++ DAY-2/js-HomeWork.js
@@ -0,0 +1,33 @@
+const myNumbers = 4 + (3 * 4) / 2;
+console.log(myNumbers);
+
+const myNumbers2 = (13 % 5) ** 2;
+console.log(myNumbers2);
+
+console.log(200 + 0 / 0);
+//----------------------------------
+let score = 5;
+score + 4;
+console.log(score);
+//----------------------------------
+let waterBoilingPoint = 100;
+waterBoilingPoint /= 2;
+waterBoilingPoint += 10;
+console.log(waterBoilingPoint);
+//----------------------------------
+const myAge = 20;
+// myAge++; //for reject the error just commented this line
+console.log(myAge);
+//----------------------------------
+isLoggedIn = true;
+console.log(isLoggedIn);
+//----------------------------------
+const myName = "Vahid Samadi";
+console.log(myName[2]); //output must be "h"
+console.log("My Name Lenght = " + myName.length + " charcacter");
+
+//----------------------------------
+const animal = "Magnificent Frigatebird";
+console.log(animal[7]); //output must be C
+
+//----------------------------------
---
+++ DAY-2/readme.md
@@ -0,0 +1,1 @@
+im start codding from first :)
