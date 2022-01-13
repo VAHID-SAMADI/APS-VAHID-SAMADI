dc3d76a77d91f4e2f54d32c78ce7744c76725612
Mr. VaHiD Samadi <93014357+VAHID-SAMADI@users.noreply.github.com>
2022-01-14 04:31:53 +0800

    commit 2

--- DAY-2/js-HomeWork.js
+++ DAY-2/js-HomeWork.js
@@ -31,3 +31,36 @@ const animal = "Magnificent Frigatebird";
 console.log(animal[7]); //output must be C
 
 //----------------------------------
+const city = "Swansea";
+const country = "Wales";
+const comb = city + country;
+
+console.log(comb); //output  = SwanseaWales
+//----------------------------------
+let year = "2000";
+year + 1;
+console.log(year); //output = 2000 //be dalil inke (year+1) dar variable zakhire nashode
+//----------------------------------
+const myName2 = "    Vahid      Samadi    ";
+console.log(myName2.toUpperCase());
+console.log(myName2.toLowerCase());
+console.log(myName2.trim());
+console.log(myName.replace('Vahid', 'VaHiD'));
+console.log(myName.repeat(10));
+
+//----------------------------------
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words[5]);
+console.log(words.slice("the"));
+
+// expected output: "fox"
+
+const chars = str.split('');
+console.log(chars[8]);
+// expected output: "k"
+
+const strCopy = str.split();
+console.log(strCopy);
+// expected output: Array ["The quick brown fox jumps over the lazy dog."]
