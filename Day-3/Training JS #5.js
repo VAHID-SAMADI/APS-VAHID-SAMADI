1d4a2cac13ca516e4659a55979cb176329209db6
Mr. VaHiD Samadi <93014357+VAHID-SAMADI@users.noreply.github.com>
2022-01-15 02:23:57 +0330

    Create Training JS #5.js

---
+++ Day-3/Training JS #5.js
@@ -0,0 +1,6 @@
+function animal(obj){
+    const obj1 = {name:"dog",legs:4,color:"white"};
+    const obj2 = {name:"cock",legs:2,color:"red"};
+    const obj3 = {name:"rabbit",legs:4,color:"gray"};
+    return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
+  }
