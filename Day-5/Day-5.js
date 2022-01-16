84d13c012fa6fa93fdbe45649dfb1d2594e0bd5b
Mr. VaHiD Samadi <93014357+VAHID-SAMADI@users.noreply.github.com>
2022-01-16 15:48:25 +0330

    add index

--- Day-5/Day-5.js
+++ Day-5/Day-5.js
@@ -1,8 +1,42 @@
-let dice1 = Math.floor(Math.random() * 6) + 1;
-let dice2 = Math.floor(Math.random() * 6) + 1;
-console.log(dice1);
-console.log(dice2);
-const roll = dice1 + dice2;
-console.log(
-  `roll : "You rolled a ${dice1} and a ${dice2}. they sum to ${roll} "`
-);
+// const dice1 = Math.floor(Math.random() * 6) + 1;
+// const dice2 = Math.floor(Math.random() * 6) + 1;
+// console.log(dice1);
+// console.log(dice2);
+// const roll = dice1 + dice2;
+// console.log(
+//   `roll : "You rolled a ${dice1} and a ${dice2}. they sum to ${roll} "`
+// );
+
+//----------------------------------
+// let whatIsYourAge = prompt("What is your age: ");
+// let age = parseInt(whatIsYourAge);
+
+// let birthday = 1400 - age;
+
+// // alert(`your birthday is ${birthday}`);
+// console.log(birthday);
+
+//-----------------------------------
+// console.log(3 >= 2);
+// console.log(9 == "9");
+// console.log(-5 >= -6);
+// let input = prompt("type a number", 2);
+// const number = parseInt(input);
+// if (number % 2 === 0) {
+//   console.log("Even");
+// } else {
+//   console.log("odd");
+// }
+//-----------------------------------
+// let trafficLight = "vahid";
+
+// if ((trafficLight === "red")) {
+//   console.log("Stop");
+// } else if ((trafficLight === "yellow")) {
+//   console.log("Slow");
+// } else if ((trafficLight === "green")) {
+//   console.log("Go");
+// } else {
+//   console.log("any other situations");
+// }
+//-----------------------------------
