ecfe96f5901f26b100fd7a097e157c9c75336488
Mr. VaHiD Samadi <93014357+VAHID-SAMADI@users.noreply.github.com>
2022-01-19 02:16:13 +0330

    Update Day-6.js

--- Day-6/Day-6.js
+++ Day-6/Day-6.js
@@ -5,9 +5,21 @@
 //   }
 
 //sum array
-// array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // sum = 0;
 // for (let i = 0; i < array.length; i++) {
 //   sum += array[i];
 //   console.log(sum);
 // }
+
+//for of
+
+// for (let arrayIterater in array) {
+//   console.log(arrayIterater);
+// }
+
+const testText = "vahid"
+
+for (let textIterater in testText){
+    console.log(testText[textIterater])
+}
