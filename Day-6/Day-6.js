27ac2af98b0b529ed25568fc00fa224aeaecaf61
Mr. VaHiD Samadi <93014357+VAHID-SAMADI@users.noreply.github.com>
2022-01-19 04:25:48 +0330

    index

--- Day-6/Day-6.js
+++ Day-6/Day-6.js
@@ -5,7 +5,7 @@
 //   }
 
 //sum array
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // sum = 0;
 // for (let i = 0; i < array.length; i++) {
 //   sum += array[i];
@@ -86,62 +86,120 @@ array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //   }
 // }
 
-const animals = [
-  "Aardvark",
-  "Aardwolf",
-  "African Elephant",
-  "African Tree Pangolin",
-  "Alligator",
-  "Alpaca",
-  "Anteater",
-  "Antelope",
-  "Ape(s)",
-  "Arabian Horse",
-  "Armadillo",
-  "Arthropod(s)",
-  "Asian Elephant",
-  "Aye-Aye",
-  "Baboon",
-  "Badger",
-  "Bandicoot",
-  "Bangle Tiger",
-  "Bat(s)",
-  "Bearded Dragon",
-  "Beaver",
-  "Beluga Whale",
-  "Bengal Tiger",
-  "Big-Horned Sheep",
-  "Billy Goat",
-  "Bird(s)",
-  "Bison",
-  "Black Bear",
-  "Black Footed Rhino",
-  "Black Howler Monkey",
-  "Black Rhino",
-  "Blacktip Reef Shark",
-  "Blue Shark",
-  "Blue Whale",
-  "Boar",
-  "Bob-Cat",
-  "Bonobo",
-  "Bottlenose Dolphin",
-  "Bottlenose Whale",
-  "Brown Bear",
-  "Buffalo",
-  "Bull",
-  "Bull frog",
-  "Caiman Lizard",
-  "Camel",
-  "Capuchin Monkey",
-  "Capybara",
-  "Caribou",
-  "Cat(s)",
-];
+// const animals = [
+//   "Aardvark",
+//   "Aardwolf",
+//   "African Elephant",
+//   "African Tree Pangolin",
+//   "Alligator",
+//   "Alpaca",
+//   "Anteater",
+//   "Antelope",
+//   "Ape(s)",
+//   "Arabian Horse",
+//   "Armadillo",
+//   "Arthropod(s)",
+//   "Asian Elephant",
+//   "Aye-Aye",
+//   "Baboon",
+//   "Badger",
+//   "Bandicoot",
+//   "Bangle Tiger",
+//   "Bat(s)",
+//   "Bearded Dragon",
+//   "Beaver",
+//   "Beluga Whale",
+//   "Bengal Tiger",
+//   "Big-Horned Sheep",
+//   "Billy Goat",
+//   "Bird(s)",
+//   "Bison",
+//   "Black Bear",
+//   "Black Footed Rhino",
+//   "Black Howler Monkey",
+//   "Black Rhino",
+//   "Blacktip Reef Shark",
+//   "Blue Shark",
+//   "Blue Whale",
+//   "Boar",
+//   "Bob-Cat",
+//   "Bonobo",
+//   "Bottlenose Dolphin",
+//   "Bottlenose Whale",
+//   "Brown Bear",
+//   "Buffalo",
+//   "Bull",
+//   "Bull frog",
+//   "Caiman Lizard",
+//   "Camel",
+//   "Capuchin Monkey",
+//   "Capybara",
+//   "Caribou",
+//   "Cat(s)",
+// ];
 
 // for (let i = 0; i < animals.length; i++) {
 //   console.log(animals[i].toLowerCase());
 // }
 
-for(let i = animals.length -1;i>=0;i--){
-    console.log(i + " : " + animals[i])
-}
+// for (let i = animals.length - 1; i >= 0; i--) {
+//   console.log(i + " : " + animals[i]);
+// }
+
+// const names = ["Sirvan", "Pourya", "Alireza", "saba", "Khorzoor"];
+
+// for (nameItrater of names) {
+//   console.log(nameItrater.toUpperCase());
+// }
+// const airplane = [
+//   ["niloofar", "kosar", "Fatemeh"],
+//   ["ali", "mohsen", "Shirin"],
+//   ["atena", "mahdi", "benyamin"],
+//   ["bani", "mohammad", "sina"],
+//   ["Amir", "Beti", "Pourya"],
+// ];
+
+// for (let i = 0; i <= airplane.length - 1; i++) {
+//   console.log(`radif ${i + 1} = ${airplane[i]}`);
+
+//   for (let j = 0; j <= airplane[i].length - 1; j++) {
+//     console.log(`  sandali   ${j + 1} - ${airplane[i][j]}`);
+//   }
+//   for (mosafer of airplane[i]) {
+//     console.log(`          ${mosafer}`);
+//   }
+// }
+// let count = 0;
+// while (count <= 10) {
+//   console.log(count);
+//   count++;
+// }
+
+//      infinit loop
+// let password = prompt(`write the password:`);
+// let step = 0;
+// while (password === "APSquery123") {
+//   password = prompt(`write the password again :P`);
+// }
+//-------------------------------------------------------
+// let repeatMe = prompt(`hey, say something`);
+
+// while (true) {
+//   repeatMe = prompt(repeatMe);
+//   if (repeatMe.toLowerCase() === "stop") {
+//     break;
+//   }
+// }
+
+// let password = prompt("1234");
+
+// console.log(password);
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+//   for (let j = 0; j < 4; j++) {
+//     console.log(`          ${j}`);
+//     if (j === 2) break;
+//   }
+// }
+
